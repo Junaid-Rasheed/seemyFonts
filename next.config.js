05ffc59cfd41f1f6cfdee67eccea0a859eeb7ab9
Junaid-Rasheed <junaidrasheed300@gmail.com>
2021-10-13 14:29:14 +0500

    fifteen  commit

--- next.config.js
+++ next.config.js
@@ -5,4 +5,14 @@ module.exports = {
   },
   target: "serverless",
   distDir: "out",
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+      config.resolve.fallback = {
+        fs: false,
+      };
+    }
+
+    return config;
+  },
 };
